9b9161add66de80d4c73be4af1e3cd2e079187b9
jacktherock <sonawaneabhijeet273@gmail.com>
2023-04-26 15:21:39 +0530

    Upgrade

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,14 +6,22 @@ const Contact = ({ data }) => {
             <h2 className="section-title">Contact</h2>
 
             <div className="contact__social bd__grid" data-aos="zoom-in-up" data-aos-offset="200" data-aos-duration="1000">
-                <a href={data.linkedin} className="contact__icon home__social-icon" rel="noreferrer" target="_blank" ><i className='bx bxl-linkedin'></i></a>
-                <a href={data.github} className="contact__icon home__social-icon" rel="noreferrer" target="_blank" ><i
-                    className='bx bxl-github'></i></a>
-                <a href={`mailto:${data.mail}`} className="contact__icon home__social-icon" ><i
-                    className='bx bxl-gmail'></i></a>
+                <a href={data.linkedin} className="contact__icon home__social-icon" rel="noreferrer" target="_blank">
+                    <i className='bx bxl-linkedin'></i>
+                </a>
+                <a href={data.github} className="contact__icon home__social-icon" rel="noreferrer" target="_blank">
+                    <i className='bx bxl-github'></i>
+                </a>
+                <a href={`mailto:${data.mail}`} className="contact__icon home__social-icon">
+                    <i className='bx bxl-gmail'></i>
+                </a>
                 <a href={data.location}
-                    className="contact__icon home__social-icon" rel="noreferrer" target="_blank" ><i className='bx bxs-map'></i></a>
-                <a href={data.siteLink} className="contact__icon home__social-icon" ><i className='bx bx-world'></i></a>
+                    className="contact__icon home__social-icon" rel="noreferrer" target="_blank">
+                    <i className='bx bxs-map'></i>
+                </a>
+                <a href={data.siteLink} className="contact__icon home__social-icon">
+                    <i className='bx bx-world'></i>
+                </a>
             </div>
         </section>
     )
